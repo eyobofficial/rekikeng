e5088e5e47fdad7ccb9f8f1401138dd31367f76b
eyob tariku <eyobtariku@gmail.com>
2018-05-05 20:18:02 +0300

    Import and setup all static assets

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from pages import views as page_views
 from django.urls import path
 
 urlpatterns = [
+    path('', page_views.IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
 ]
